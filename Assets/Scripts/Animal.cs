7e30a17d63cddf35e809d08ed4e8175db70b2c8b
Júlia D. Craide <juliadcraide@hotmail.com>
2023-10-15 19:16:52 -0300

    Finish filing animals data

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -20,16 +20,12 @@ public enum Symmetry {
     Radial, Bilateral, None
 }
 
-public enum DigestiveSystem {
-    Complete, Incomplete, None
-}
-
 public enum Skeleton {
-    Endoskeleton, Exoskeleton, Shell, None
+    Endoskeleton, Exoskeleton, Both, None
 }
 
 public enum Reproduction {
-    Sexually, Asexually
+    Sexually, Asexually, Both
 }
 
 public enum Fecundation {
@@ -43,11 +39,11 @@ public enum Blood {
 }
 
 public enum Eggs {
-    Oviparous, Ovoviviparous, Viviparous, None
+    Oviparous, Ovoviviparous, Viviparous //Ovuliparous
 }
 
 public enum BodyCoverage {
-    Fur, Feathers, Scales, None
+    Fur, Feathers, Scales, Shell, None
 }
 public class Animal : MonoBehaviour {
     public int id;
@@ -55,6 +51,7 @@ public class Animal : MonoBehaviour {
     public string scientificName;
     public bool vertebrate;
     public bool fly;
+    public bool venomous;
     public Blood blood;
     public Eggs eggs;
     public BodyCoverage bodyCoverage;
@@ -62,7 +59,6 @@ public class Animal : MonoBehaviour {
     public EmbryonicLeaflet embryonicLeaflet;
     public MouthOrigin mouthOrigin;
     public Symmetry symmetry;
-    public DigestiveSystem digestiveSystem;
     public Skeleton skeleton;
     public Reproduction reproduction;
     public Fecundation fecundation;
@@ -77,6 +73,7 @@ public class Animal : MonoBehaviour {
         string scientificName,
         bool vertebrate,
         bool fly,
+        bool venomous,
         Blood blood,
         Eggs eggs,
         BodyCoverage bodyCoverage,
@@ -84,7 +81,6 @@ public class Animal : MonoBehaviour {
         EmbryonicLeaflet embryonicLeaflet,
         MouthOrigin mouthOrigin,
         Symmetry symmetry,
-        DigestiveSystem digestiveSystem,
         Skeleton skeleton,
         Reproduction reproduction,
         Fecundation fecundation
@@ -94,15 +90,15 @@ public class Animal : MonoBehaviour {
         this.scientificName = scientificName;
         this.coelomate = coelomate;
         this.vertebrate = vertebrate;
+        this.venomous = venomous;
+        this.fly = fly;
         this.blood = blood;
         this.eggs = eggs;
-        this.fly = fly;
         this.bodyCoverage = bodyCoverage;
         this.coelomate = coelomate;
         this.embryonicLeaflet = embryonicLeaflet;
         this.mouthOrigin = mouthOrigin;
         this.symmetry = symmetry;
-        this.digestiveSystem = digestiveSystem;
         this.skeleton = skeleton;
         this.reproduction = reproduction;
         this.fecundation = fecundation;
@@ -114,6 +110,7 @@ public class Animal : MonoBehaviour {
         string scientificName,
         bool vertebrate,
         bool fly,
+        bool venomous,
         Blood blood,
         Eggs eggs,
         BodyCoverage bodyCoverage,
@@ -121,7 +118,6 @@ public class Animal : MonoBehaviour {
         EmbryonicLeaflet embryonicLeaflet,
         MouthOrigin mouthOrigin,
         Symmetry symmetry,
-        DigestiveSystem digestiveSystem,
         Skeleton skeleton,
         Reproduction reproduction,
         Fecundation fecundation
@@ -131,15 +127,15 @@ public class Animal : MonoBehaviour {
         this.scientificName = scientificName;
         this.coelomate = coelomate;
         this.vertebrate = vertebrate;
+        this.venomous = venomous;
+        this.fly = fly;
         this.blood = blood;
         this.eggs = eggs;
-        this.fly = fly;
         this.bodyCoverage = bodyCoverage;
         this.coelomate = coelomate;
         this.embryonicLeaflet = embryonicLeaflet;
         this.mouthOrigin = mouthOrigin;
         this.symmetry = symmetry;
-        this.digestiveSystem = digestiveSystem;
         this.skeleton = skeleton;
         this.reproduction = reproduction;
         this.fecundation = fecundation;
@@ -178,6 +174,10 @@ public class Animal : MonoBehaviour {
         return c.fly;
     }
 
+     public static bool Venomous(Animal c) {
+        return c.venomous;
+    }
+
     public static bool BloodEndotherms(Animal c) {
         return c.blood == Blood.Endotherms;
     }
@@ -207,6 +207,10 @@ public class Animal : MonoBehaviour {
     public static bool BodyCoverageScales(Animal c) {
         return c.bodyCoverage == BodyCoverage.Scales;
     }
+    public static bool BodyCoverageShell(Animal c) {
+        return c.bodyCoverage == BodyCoverage.Shell;
+    }
+
     public static bool CoelomateAcoelomate(Animal c) {
         return c.coelomate == Coelomate.Acoelomate;
     }
@@ -223,18 +227,12 @@ public class Animal : MonoBehaviour {
     public static bool EmbryonicLeafletTriblastic(Animal c) {
         return c.embryonicLeaflet == EmbryonicLeaflet.Triblastic;
     }
-    public static bool EmbryonicLeafletNone(Animal c) {
-        return c.embryonicLeaflet == EmbryonicLeaflet.None;
-    }
 
     public static bool MouthOriginProtostome(Animal c) {
         return c.mouthOrigin == MouthOrigin.Protostome;
     }
     public static bool MouthOriginDeuterostome(Animal c) {
         return c.mouthOrigin == MouthOrigin.Deuterostome;
-    }
-    public static bool MouthOriginNone(Animal c) {
-        return c.mouthOrigin == MouthOrigin.None;
     }
 
     public static bool SymmetryRadial(Animal c) {
@@ -243,21 +241,6 @@ public class Animal : MonoBehaviour {
     public static bool SymmetryBilateral(Animal c) {
         return c.symmetry == Symmetry.Bilateral;
     }
-    public static bool SymmetryNone(Animal c) {
-        return c.symmetry == Symmetry.None;
-    }
-
-    public static bool DigestiveSystemIncomplete(Animal c) {
-        return c.digestiveSystem == DigestiveSystem.Incomplete;
-    }
-
-    public static bool DigestiveSystemComplete(Animal c) {
-        return c.digestiveSystem == DigestiveSystem.Complete;
-    }
-
-    public static bool DigestiveSystemNone(Animal c) {
-        return c.digestiveSystem == DigestiveSystem.None;
-    }
 
     public static bool SkeletonExoskeleton(Animal c) {
         return c.skeleton == Skeleton.Exoskeleton;
@@ -265,18 +248,15 @@ public class Animal : MonoBehaviour {
     public static bool SkeletonEndoskeleton(Animal c) {
         return c.skeleton == Skeleton.Endoskeleton;
     }
-    public static bool SkeletonShell(Animal c) {
-        return c.skeleton == Skeleton.Shell;
-    }
-    public static bool SkeletonNone(Animal c) {
-        return c.skeleton == Skeleton.None;
-    }
 
     public static bool ReproductionSexually(Animal c) {
         return c.reproduction == Reproduction.Sexually;
     }
     public static bool ReproductionAsexually(Animal c) {
         return c.reproduction == Reproduction.Asexually;
+    }
+    public static bool ReproductionBoth(Animal c) {
+        return c.reproduction == Reproduction.Both;
     }
 
     public static bool FecundationInternal(Animal c) {
--- Assets/Scripts/StaticGameResume.cs
+++ Assets/Scripts/StaticGameResume.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public static class StaticGameResume {
     public static List<Question> askedQuestions = new List<Question> {
         new Question("Is your animal a Bilaterally Symmetrical?", Animal.SymmetryBilateral),
-        new Question("Does your animal have an Incomplete Digestive System?", Animal.DigestiveSystemIncomplete),
-        new Question("Does your animal have a Complete Digestive System?", Animal.DigestiveSystemComplete),
         new Question("Does your animal have a Exoskeleton?", Animal.SkeletonExoskeleton),
         new Question("Does your animal have a Endoskeleton?", Animal.SkeletonEndoskeleton),
-        new Question("Does your animal have a Shell?", Animal.SkeletonShell),
+        new Question("Does your animal have a Shell?", Animal.BodyCoverageShell),
         new Question("Does your animal Reproduce Sexually?", Animal.ReproductionSexually),
         new Question("Does your animal Reproduce Asexually?", Animal.ReproductionAsexually),
         new Question("Is your animal a Fecundation Internal?", Animal.FecundationInternal),
         new Question("Is your animal a Fecundation External?", Animal.FecundationExternal),
     };
 
-    public static Animal guessedAnimal = new Animal(1, "Human", "SN", true, false, Blood.Endotherms, Eggs.None, BodyCoverage.Fur, Coelomate.Coelomate, EmbryonicLeaflet.Triblastic, MouthOrigin.Deuterostome, Symmetry.Bilateral, DigestiveSystem.Complete, Skeleton.Endoskeleton, Reproduction.Sexually, Fecundation.Internal);
-    public static Animal correctAnimal = new Animal(3, "Human", "SN", true, false, Blood.Endotherms, Eggs.None, BodyCoverage.Fur, Coelomate.Coelomate, EmbryonicLeaflet.Triblastic, MouthOrigin.Deuterostome, Symmetry.Bilateral, DigestiveSystem.Complete, Skeleton.Endoskeleton, Reproduction.Sexually, Fecundation.Internal);
+    public static Animal guessedAnimal = new Animal(1, "Morcego", "Pteropus giganteus", true, true, false, Blood.Endotherms, Eggs.Viviparous, BodyCoverage.Fur, Coelomate.Coelomate, EmbryonicLeaflet.Triblastic, MouthOrigin.Deuterostome, Symmetry.Bilateral, Skeleton.Endoskeleton, Reproduction.Sexually, Fecundation.Internal);
+    public static Animal correctAnimal = new Animal(12, "Água viva", "Chrysaora fuscescens", false, false, false, Blood.None, Eggs.Oviparous, BodyCoverage.None, Coelomate.Acoelomate, EmbryonicLeaflet.Driblastic, MouthOrigin.None, Symmetry.Radial, Skeleton.None, Reproduction.Both, Fecundation.External);
 
     public static void setData(List<Question> askedQuestions, Animal guessedAnimal, Animal correctAnimal) {
         StaticGameResume.askedQuestions = askedQuestions;
